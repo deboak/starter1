b6abb37b1304c130eea54f02110e3956dc7e638f
deboak <adebowaleakinode9@gmail.com>
2025-06-26 19:59:51 +0100

    vercel setup

---
+++ api/server.js
@@ -0,0 +1,16 @@
+// api/server.js
+const serverless = require('serverless-http');
+const app = require('../app');
+const connectDB = require('../db/connect');
+
+let isConnected = false;
+
+const handler = async (req, res) => {
+  if (!isConnected) {
+    await connectDB(process.env.MONGO_URI);
+    isConnected = true;
+  }
+  return serverless(app)(req, res);
+};
+
+module.exports.handler = handler;
