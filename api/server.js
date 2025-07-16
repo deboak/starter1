059a17dfa85aaf4290af29d2af2c611b7168d2ae
deboak <adebowaleakinode9@gmail.com>
2025-07-16 13:31:09 +0100

    trying a fix

--- api/server.js
+++ api/server.js
@@ -1,17 +1,18 @@
-// api/server.js
 const serverless = require('serverless-http');
 const app = require('../app');
 const connectDB = require('../db/connect');
+
 let isConnected = false;
-
-
+const handlerWrapped = serverless(app); // wrap ONCE
 
 const handler = async (req, res) => {
   if (!isConnected) {
+    console.log("Connecting to MongoDB...");
     await connectDB(process.env.MONGO_URI);
     isConnected = true;
+    console.log("Connected.");
   }
-  return serverless(app)(req, res);
+  return handlerWrapped(req, res);
 };
 
 module.exports = handler;
